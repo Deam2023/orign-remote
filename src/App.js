3414ebd6dc3874db57d9e6813ce1214eb3d90e66
Deam2023 <151362357+Deam2023@users.noreply.github.com>
2024-07-26 20:23:30 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Form from "./component/Form";
+import FilterButton from "./component/FilterButton";
+import Todo from "./component/Todo";
+
+function App(props) {
+  const taskList = props.tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form />
+      <div className="filters btn-group stack-exception">
+        <FilterButton name="All"/>
+        <FilterButton name="Finished"/>
+        <FilterButton name="To do"/>
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
